3b34d5da44b889b93170dd2c4b3f33708969e91c
fsocket-crackwd <66283644+fsocket-crackwd@users.noreply.github.com>
2020-06-01 21:04:52 -0300

    Add files via upload

---
+++ composition_v1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+// projeto v.0.1 dados iniciais para gerenciamento de sistema //
+
+void main(){
+
+	char user_choise[30];
+	system("clear");
+	printf("\n#Remover/Atualizar/Instalar programas(1)\n#Criar/Gerenciar/Remover pastas(2)");
+	printf("\nEscolha a opção deseja para iniciar: "); scanf("%c", &user_choise);
+
+
+}
